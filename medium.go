a0415fe2ceb5890ec44661590facbd2d166e52bd
Hoanh An <hoanhan@bennington.edu>
2018-06-15 10:52:03 -0400

    update to secured routes

--- medium.go
+++ medium.go
@@ -13,11 +13,12 @@ import (
 
 	ghandlers "github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"github.com/justinas/alice"
 )
 
 const (
-	// Web server port.
-	PORT = ":8080"
+	// TLS port.
+	PORT = ":8443"
 )
 
 func main() {
@@ -38,20 +39,20 @@ func main() {
 	// Create a new mux router.
 	r := mux.NewRouter()
 
-	// Core routes.
-	r.Handle("/signup", handlers.SignUpHandler(&env)).Methods("GET", "POST")
-
+	// Global routes.
 	r.HandleFunc("/", handlers.HomeHandler)
-	r.HandleFunc("/register", handlers.RegisterHandler).Methods("GET", "POST")
-	r.HandleFunc("/postpreview", handlers.PostPreviewHandler).Methods("GET", "POST")
-	r.HandleFunc("/login", handlers.LoginHandler).Methods("POST")
-	r.HandleFunc("/logout", handlers.LogoutHandler).Methods("POST")
-	r.HandleFunc("/feed", handlers.FeedHandler).Methods("GET")
-	r.HandleFunc("/friends", handlers.FriendsHandler).Methods("GET")
-	r.HandleFunc("/find", handlers.FindHandler).Methods("GET", "POST")
-	r.HandleFunc("/profile", handlers.MyProfileHandler).Methods("GET")
-	r.HandleFunc("/profile/{username}", handlers.ProfileHandler).Methods("GET")
-	r.HandleFunc("/upload/image", handlers.UploadImageHandler).Methods("GET", "POST")
+	r.Handle("/signup", handlers.SignUpHandler(&env)).Methods("GET", "POST")
+	r.Handle("/login", handlers.LoginHandler(&env)).Methods("GET", "POST")
+	r.HandleFunc("/logout", handlers.LogoutHandler).Methods("GET", "POST")
+
+	// Gated routes that are only available for authenticated users.
+	r.Handle("/feed", middleware.GatedContentHandler(handlers.FeedHandler)).Methods("GET")
+	r.Handle("/friends", middleware.GatedContentHandler(handlers.FriendsHandler)).Methods("GET")
+	r.Handle("/find", middleware.GatedContentHandler(handlers.FindHandler)).Methods("GET", "POST")
+	r.Handle("/profile", middleware.GatedContentHandler(handlers.MyProfileHandler)).Methods("GET")
+	r.Handle("/profile/{username}", middleware.GatedContentHandler(handlers.ProfileHandler)).Methods("GET")
+	r.Handle("/upload/image", middleware.GatedContentHandler(handlers.UploadImageHandler)).Methods("GET", "POST")
+	r.Handle("/postpreview", middleware.GatedContentHandler(handlers.PostPreviewHandler)).Methods("GET", "POST")
 
 	// Temporary routes simulate different scenarios that are handled by
 	// middleware functions:
@@ -66,15 +67,21 @@ func main() {
 	r.HandleFunc("/api/{postid}", endpoints.UpdatePost).Methods("PUT")
 	r.HandleFunc("/api/{postid}", endpoints.DeletePost).Methods("DELETE")
 
-	// ghandlers.LoggingHandler(os.Stdout, r) is the default gorilla's logging
-	// handler. middleware.RecoverPanicHandler() chains the ghandlers to catch
-	// any panic causes. Finally, middleware.ContextHandler persists the
-	// context value, which is foo in this situation.
-	http.Handle("/", middleware.ContextHandler(middleware.RecoverPanicHandler(ghandlers.LoggingHandler(os.Stdout, r))))
-
 	// Fix path to static folder.
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-	// Pass the context value through the request.
-	http.ListenAndServe(PORT, nil)
+	// ghandlers.LoggingHandler(os.Stdout, r) is the default gorilla's logging
+	// handler. middleware.RecoverPanicHandler() chains the ghandlers to catch
+	// any panic causes. Finally, middleware.ContextHandler persists the
+	// context value, which is foo in this situation. Instead of
+	// http.Handle("/", middleware.ContextHandler(middleware.RecoverPanicHandler(ghandlers.LoggingHandler(os.Stdout, r))))
+	// can use alice package to chain middle function in a pretty way.
+	loggedRouter := ghandlers.LoggingHandler(os.Stdout, r)
+	stdChain := alice.New(middleware.RecoverPanicHandler)
+	http.Handle("/", stdChain.Then(loggedRouter))
+
+	err = http.ListenAndServeTLS(PORT, "certs/mediumcert.pem", "certs/mediumkey.pem", nil)
+	if err != nil {
+		log.Fatal("ListenAndServeTLS: ", err)
+	}
 }
